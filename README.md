8d9291f51b47fefadaaf509b8ac496e420cd8e4e
Deiklov <anromanov@team.amocrm.com>
2021-04-09 13:28:46 +0300

    with screen

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tp_crypt
+# tp_crypt  
+![Screenshot](Task.png)
+**Выполнил:** Романов Андрей  
+**Запуск:** go run program --file <filename> --numbilets 20 --parameter 42  
+или ./program --file <filename> --numbilets 20 --parameter 42  
+бинарь лежит в репозитории  
--- program.go
+++ program.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha256"
 	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
+	"sort"
 )
 
 func main() {
 
 	// to save the 'name' option value
 	var fileName = flag.String("file", "program", "The name of input files")
-	var numBilets = flag.Int64("numbilets", 20, "Count of tickets for students")
-	var randSeed = flag.Int64("parameter", 42, "Count of tickets for students")
+	var numBilets = flag.Int("numbilets", 20, "Count of tickets for students")
+	var randSeed = flag.Int("parameter", 42, "Count of tickets for students")
 
 	// parse flag's options
 	flag.Parse()
@@ -28,24 +28,43 @@ func main() {
 	defer f.Close()
 
 	sc := bufio.NewScanner(f)
+	arrFIO := []string{}
 	for sc.Scan() {
 		currStudent := sc.Text()
-		ticketNumber, err := genTicket(currStudent, *randSeed, *numBilets)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("%s: %d", currStudent, ticketNumber)
+		arrFIO = append(arrFIO, currStudent)
 	}
 	if err := sc.Err(); err != nil {
 		log.Fatalf("scan file error: %v", err)
 		return
 	}
+	ticketsMAP, err := genTicket(arrFIO, *randSeed, *numBilets)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//имя это index
+	for i, v := range ticketsMAP {
+		fmt.Printf("%s: %d \n", i, v)
+	}
 
 }
-func genTicket(studFIO string, seed int64, numTickets int64) (int64, error) {
+func genTicket(studFIOs []string, seed int, numTickets int) (map[string]int, error) {
 	//все имена будут под одной маской
-	hashName := sha256.Sum256([]byte(studFIO))
+	response := make(map[string]int, len(studFIOs))
+
+	sort.Strings(studFIOs)
+	rand.Seed(int64(seed))
+
+	ticketsArray := func() []int {
+		arr := make([]int, len(studFIOs))
+		for i := 0; i < len(studFIOs); i++ {
+			arr[i] = rand.Intn(numTickets)+1
+		}
+		return arr
+	}()
+
+	for i, v := range ticketsArray {
+		response[studFIOs[i]] = v
+	}
 	//создали сид для рандома
-	rand.Seed(seed)
-	return 0, nil
+	return response, nil
 }
